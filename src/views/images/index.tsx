f2f0876e3f141e312874b8b7276a251bf54daa95
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-05-08 09:12:13 +0800

    images

--- src/views/images/index.tsx
+++ src/views/images/index.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import Avatar from '../../assets/images/img.jpg';
+
 const Index: React.FC = () => {
   return (
     <div className="images-container log">
       <div className="gallery log">
-        <p>Hello</p>
+        <div className="divide-gallery-3">
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
+        </div>
+        <div className="divide-gallery-2">
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
+        </div>
+        <div className="divide-gallery-1">
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
+        </div>
       </div>
       <div className="button-navigation-container">
         <Link to="/profile">
