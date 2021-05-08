31509087988b3dfe8d3819f46bc6f371857cc35e
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-05-08 09:37:29 +0800

    Images flex

--- src/views/images/index.tsx
+++ src/views/images/index.tsx
@@ -6,14 +6,20 @@ import Avatar from '../../assets/images/img.jpg';
 
 const Index: React.FC = () => {
   return (
-    <div className="images-container log">
-      <div className="gallery log">
+    <div className="images-container">
+      <div className="gallery">
         <div className="divide-gallery-3">
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
           <div className="ximg">
             <img src={Avatar} alt="saber" />
           </div>
         </div>
         <div className="divide-gallery-2">
+          <div className="ximg">
+            <img src={Avatar} alt="saber" />
+          </div>
           <div className="ximg">
             <img src={Avatar} alt="saber" />
           </div>
